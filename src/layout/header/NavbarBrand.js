32b49d5e94d19ced761172bf84fcad342979ab1d
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-20 12:15:34 +0100

    fix NavbarBrand styling

--- src/layout/header/NavbarBrand.js
+++ src/layout/header/NavbarBrand.js
@@ -1,7 +1,7 @@
 import LogoImg from './handshake.svg'
 const NavbarBrand = () => {
     return (
-        <div>
+        <div className="navbar-brand flex items-start text-purple-700">
             <img src={LogoImg} alt="Hello" className="w-7 h-7" />
             <h1 className="text-purple-700 ml-1">Helping<span className="text-black">Hands</span></h1>
         </div>
