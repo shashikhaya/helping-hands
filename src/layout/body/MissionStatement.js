14d510f4cfd1c42b608b48f605e407b8c40c6426
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-20 11:49:38 +0100

    style MissionStatement

--- src/layout/body/MissionStatement.js
+++ src/layout/body/MissionStatement.js
@@ -2,10 +2,14 @@ import Img from './MissionStatement-img.svg'
 
 const MissionStatement = () => {
     return (
-        <div>
-            <h1>Our mission statement</h1>
-            <h2>Subtitle: Make the world a better place, one hand at a time ;)</h2>
-            <img src={Img} alt="globe"/>
+        <div class="grid grid-cols-2 gap-4">
+            <div className="my-auto">
+                <h1 className="text-4xl font-bold text-purple-700">Our mission statement</h1>
+                <p className="text-3xl font-semibold">Make the world a better place, one hand at a time ;)</p>
+            </div>
+            <div className="my-auto">
+                <img src={Img} alt="globe" className="w-4/6	h-4/6"/>
+            </div>
         </div>
     )
 }
