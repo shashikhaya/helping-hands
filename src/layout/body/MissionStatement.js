467b04a28c37fc5419c343d941139637de3633c7
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-20 12:05:27 +0100

    style MissionStatement

--- src/layout/body/MissionStatement.js
+++ src/layout/body/MissionStatement.js
@@ -7,11 +7,11 @@ const MissionStatement = () => {
                 <h1 className="text-4xl font-bold text-purple-700">Our mission statement</h1>
                 <p className="text-3xl font-semibold">Make the world a better place, one hand at a time ;)</p>
             </div>
-            <div className="my-auto">
+            <div className="mx-auto">
                 <img src={Img} alt="globe" className="w-4/6	h-4/6"/>
             </div>
         </div>
     )
 }
 
-export default MissionStatement 
+export default MissionStatement;
