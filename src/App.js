6114573248c87c270977fe081888367850239025
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-21 15:37:19 +0100

    add router to App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import Header from './layout/header/Header'
-import Footer from './layout/footer/Footer'
-import LandingInfo from './layout/body/LandingInfo'
-import { AccountBox } from "./components/accountBox"
 import { useState, useEffect } from 'react'
-import styled from "styled-components"
-
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LoginButton = styled.button`
-width: 100%;
-padding: 11px 40%;
-color: #fff;
-font-size: 15px;
-font-weight: 600;
-border-radius: 100px 100px 100px 100px;
-cursor: pointer;
-transition: all, 240ms ease-in-out;
-background: rgb(109,40,217);
-
-&:hover {
-  opacity: 70%;
-}
-`;
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './layout/header/Header';
+import LandingInfo from './layout/body/LandingInfo';
+import Footer from './layout/footer/Footer';
+import TaskList from './tasks/TaskList';
+import TaskDetail from './tasks/TaskDetail';
+import { AccountBox } from "./components/accountBox"
 
 function App() {
   const [show, setShow] = useState(false);
@@ -38,16 +15,28 @@ function App() {
   }
   
   return (
-    <div className="flex flex-col h-screen">
-      
-      <Header handleClick={handleClick}/>
-      <AccountBox onClose={() => setShow(false)} show={show} />
-      <div className="container mx-auto mb-auto px-8">
-        <LandingInfo />
+    <Router>
+      <div className="flex flex-col h-screen">
+        <Header handleClick={handleClick} />
+        <AccountBox onClose={() => setShow(false)} show={show} />
+        <div className="container mx-auto mb-auto px-8">
+          <Switch>
+            <Route exact path="/">
+              <LandingInfo />
+            </Route>
+            <Route exact path="/tasks">
+              <TaskList />
+            </Route>
+            <Route path="/task/:id">
+              <TaskDetail />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
