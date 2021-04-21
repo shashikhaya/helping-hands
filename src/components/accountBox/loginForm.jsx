6accf25cf23e700e6d7d77b83b6e69d2c4b9650c
Matt <mjpbaines@sky.com>
2021-04-21 16:32:48 +0100

    user helper registration toggle

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -16,7 +16,7 @@ export function LoginForm(props) {
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="email" placeholder="Email" />
+        <Input type="username" placeholder="Username" />
         <Input type="password" placeholder="Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
