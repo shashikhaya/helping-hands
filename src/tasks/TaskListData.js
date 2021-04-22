a2bb646bede3f4a7a64d027b3b7535ea45078cd2
sixianchen17 <sixian.chen17@gmail.com>
2021-04-22 13:45:36 +0100

    Update TaskListData

--- src/tasks/TaskListData.js
+++ src/tasks/TaskListData.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react';
 //import './Task';
 
 class TaskListData extends Component {
-  state = {task: []}
+  getInitialState() {
+    return {
+      clientToken: false,
+      loaded: false
+    }
+  }
 //fetch data from Tasks database/route?
-  componentDidMount() {
-    fetch('/Tasks')
-      .then(res => res.json())
-      .then(task => this.setState({ task }));
+  componentWillMount(){
+    fetch('localhost:3000/Tasks', {
+      method: 'GET', 
+    headers: new Headers({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IldoaXRuZXkiLCJyb2xlIjoiaGVscGVyIiwibG9jYXRpb24iOnsidHlwZSI6IlBvaW50IiwiY29vcmRpbmF0ZXMiOls0Mi43ODksMTIuOTgyXX0sImlhdCI6MTYxOTA4NjMwNX0.WLPqosTMPJBaIcpdUZQ0IEl_27JakGzyqs9pVT3gzFQ', 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }), 
+  })
+      .then((response) => response.json())
+      .then((responseData) => {
+          console.log(responseData);
+      //   this.setState({
+      //     clientToken: responseData.access_token,
+      //   });
+      })
+      .then(() => {
+       this.getPosts();
+      })
+  }
+
+  getPosts(){
+    var obj = { 
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Origin': '',
+          'Host': 'localhost:3000'
+        },
+    }
+    fetch('localhost:3000/Tasks', obj)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+      })
   }
 //Return taskName, taskType, status, description, location, duration, covidInfo
   //display all results with user input location
@@ -15,9 +51,6 @@ render() {
     return (
       <div className="TaskListData">
         <h1>Tasks</h1>
-        {this.state.task.map(task =>
-          <div key={task.id}>{task.taskName}</div>
-        )}
       </div>
     );
   }
