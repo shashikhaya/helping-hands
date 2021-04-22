da80c64e472364724dd8860d46eb3fe192dcf857
Shashi <shashi.k.parekh@bath.edu>
2021-04-22 22:12:39 +0100

    convert location coordinates into ward and region

---
+++ src/_helpers/getPostcode.js
@@ -0,0 +1,8 @@
+const getPostcode = async (lng,lat) =>{
+    const locResponse = await fetch(`https://postcodes.io/postcodes?lon=${lng}&lat=${lat}`)
+    const location = await locResponse.json()
+    return `${location.result[0].admin_ward},${location.result[0].admin_district}`
+
+  }
+
+export default getPostcode;
