897e5e6dd9607285347d0915825438ba31599fe2
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-22 11:40:58 +0100

    fix Authorization header bug

--- src/_helpers/authHeader.js
+++ src/_helpers/authHeader.js
@@ -3,7 +3,7 @@ const authHeader = () => {
     const token = JSON.parse(localStorage.getItem('token'));
 
     if (token) {
-        return { 'Authorization': `Bearer: ${token}` };
+        return { 'Authorization': `Bearer ${token}` };
     }
 
     return {};
