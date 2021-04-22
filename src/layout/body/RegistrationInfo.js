bf04949f7a7cbb293de02233aa2b01fc4fe00e20
Matt <mjpbaines@sky.com>
2021-04-22 16:15:28 +0100

    class changed to classname

--- src/layout/body/RegistrationInfo.js
+++ src/layout/body/RegistrationInfo.js
@@ -1,50 +1,50 @@
 const RegistrationForm = ({ handleClick, title, text, buttonText, statement1, statement2, statement3, statement4, statement5 }) => {
     return (
         <div class="p-4">
-            <p class="text-gray-800 text-xl font-medium mb-4">
+            <p className="text-gray-800 text-xl font-medium mb-4">
                 { title }
             </p>
-            <p class="text-gray-600 text-xs mt-4">
+            <p className="text-gray-600 text-xs mt-4">
                 { text }
             </p>
-            <ul class="text-sm text-gray-600 w-full my-6">
+            <ul className="text-sm text-gray-600 w-full my-6">
                 <li class="mb-3 flex items-center ">
-                    <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                    <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
                         <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
                         </path>
                     </svg>
                     { statement1 }
                 </li>
-                <li class="mb-3 flex items-center ">
+                <li className="mb-3 flex items-center ">
                     <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
                         <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
                         </path>
                     </svg>
                     { statement2 }
                 </li>
-                <li class="mb-3 flex items-center ">
-                    <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                <li className="mb-3 flex items-center ">
+                    <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
                         <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
                         </path>
                     </svg>
                     { statement3 }
                 </li>
-                <li class="mb-3 flex items-center ">
-                    <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                <li className="mb-3 flex items-center ">
+                    <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
                         <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
                         </path>
                     </svg>
                     { statement4 }
                 </li>
-                <li class="mb-3 flex items-center ">
-                    <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                <li className="mb-3 flex items-center ">
+                    <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
                         <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
                         </path>
                     </svg>
                     { statement5 }
                 </li>
             </ul>
-            <button class="btn btn-purple w-full" onClick={handleClick}>
+            <button className="btn btn-purple w-full" onClick={handleClick}>
                 { buttonText }
             </button>
         </div>
--- src/tasks/Task.js
+++ src/tasks/Task.js
@@ -2,37 +2,37 @@ import { Link } from 'react-router-dom';
 
 const Task = ({ id, title, description, status, type, location, duration, covidInfo }) => {
     return (
-        <div class="rounded border border-gray overflow-hidden p-4 mb-4">
-            <div class="flex justify-between">
+        <div className="rounded border border-gray overflow-hidden p-4 mb-4">
+            <div className="flex justify-between">
                 <div>
-                    <h1 class="text-gray-900 font-bold text-2xl">
-                        <span class="text-purple-700">
+                    <h1 className="text-gray-900 font-bold text-2xl">
+                        <span className="text-purple-700">
                             { title }
                         </span>
-                        <span class="text-gray-400">
+                        <span className="text-gray-400">
                             &nbsp;/ { type }
                         </span>
                     </h1>
-                    <p class="mt-2 text-base">
+                    <p className="mt-2 text-base">
                         { description }
                     </p>
-                    <h1 class="mt-2 font-bold text-xl">
+                    <h1 className="mt-2 font-bold text-xl">
                         { location }
                     </h1>
-                    <p class="mt-2 text-lg">
+                    <p className="mt-2 text-lg">
                         { duration }
                     </p>
                 </div>
                 <div>
-                    <p class="mt-2 font-bold text-gray-400 text-xl">
+                    <p className="mt-2 font-bold text-gray-400 text-xl">
                         { status }
                     </p>
-                    <p class="mt-2 font-bold text-xl">
+                    <p className="mt-2 font-bold text-xl">
                         { covidInfo }
                     </p>
                 </div>
             </div>
-            <div class="float-right mb-1">
+            <div className="float-right mb-1">
                 <Link to={{ pathname: '/task/'+ id}} className="btn btn-purple">
                     More Info
                 </Link>
