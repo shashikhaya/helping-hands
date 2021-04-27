1bd0ffae8c45b0da758654c3aaf81f812c1f36ec
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-27 20:39:56 +0100

    fix Unknown DOM property console warnings

--- src/components/TaskSmall.js
+++ src/components/TaskSmall.js
@@ -1,7 +1,7 @@
 const TaskSmall = () => {
     return (
         <div className="flex-1 rounded border border-gray overflow-hidden p-4 mb-4 f">
-            <h3 class="text-gray-900 font-bold text-m">
+            <h3 className="text-gray-900 font-bold text-m">
                 <span className="text-purple-700">title</span>
                 <span className="text-gray-400">&nbsp;/ type </span>
             </h3>
