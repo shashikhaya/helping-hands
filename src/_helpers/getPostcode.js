006d11c849696123645a2f959d505638fcab628f
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-07-04 20:47:30 +0100

    format getPostcode

--- src/_helpers/getPostcode.js
+++ src/_helpers/getPostcode.js
@@ -1,8 +1,9 @@
-const getPostcode = async (lng,lat) =>{
-    const locResponse = await fetch(`https://postcodes.io/postcodes?lon=${lng}&lat=${lat}`)
-    const location = await locResponse.json()
-    return `${location.result[0].admin_ward},${location.result[0].admin_district}`
-
-  }
+const getPostcode = async (lng, lat) => {
+  const locResponse = await fetch(
+    `https://postcodes.io/postcodes?lon=${lng}&lat=${lat}`
+  );
+  const location = await locResponse.json();
+  return `${location.result[0].postcode}, ${location.result[0].admin_district}`;
+};
 
 export default getPostcode;
