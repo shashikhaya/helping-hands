05180a4cc20e2243edc92efe0f5de289c8f71bb6
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-07-04 20:42:54 +0100

    format TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,51 +1,23 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Task from './Task';
-import { tasksService } from '../_services';
+import { Link } from "react-router-dom";
+import Task from "./Task";
 
-
-const TaskList = () => {
-    // this state will come from higher up
-    const [tasks, setTasks] = useState([]);
-
-    // just mock data for now
-    useEffect(() => {
-        tasksService.getTasks()
-        .then(
-          (tasks) => {
-                setTasks(tasks);
-            },
-            error => console.log(error)
-        );
-    }, []);
-
-    const TasksComponents = tasks.map((task) => (
-        <>
-            <Task
-                key={task._id}
-                id={task._id}
-                title={task.taskName}
-                description={task.description}
-                status={task.status}
-                type={task.taskType}
-                location={task.location.coordinates}
-                duration={task.duration}
-                covidInfo={task.covidInfo}
-            />
-        </>
-    ));
-
-    return (
-        <>
-            <div className="flex">
-                <Link to={{ pathname: '/tasks/post'}} className="btn btn-gray text-center w-full">
-                    +
-                </Link>
-            </div>
-            <p>Showing {tasks.length} tasks </p>
-            {TasksComponents}
-        </>
-    )
-}
+const TaskList = ({ tasks }) => {
+  return (
+    <>
+      <div className="flex">
+        <Link
+          to={{ pathname: "/tasks/post" }}
+          className="btn btn-gray text-center w-full"
+        >
+          +
+        </Link>
+      </div>
+      <p>Showing {tasks.length} tasks </p>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} />
+      ))}
+    </>
+  );
+};
 
 export default TaskList;
