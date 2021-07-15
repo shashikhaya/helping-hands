9bc1f6e632da21ebccdae5377bf8d7a01697c189
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-07-15 23:07:02 +0100

    delete Button

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ text, handleClick }) => {
-    return (
-        <button className="btn btn-purple" onClick={handleClick}>
-            { text }
-        </button>
-    )
-}
-
-export default Button;
